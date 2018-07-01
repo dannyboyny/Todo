58d8c723e3c6c80afc30039e10bfee42d4fa733b
Daniel Hernandez <Daniel@Cennias-MBP.home>
2018-07-01 19:59:18 -0400

    Update Todo
    Added UpdateMutation to change the status of the Todo to true or false
    via the React UI.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const TodosQuery = gql`
   {
@@ -12,16 +20,83 @@ const TodosQuery = gql`
   }
 `;
 
+const UpdateMutation = gql`
+  mutation($id: ID!, $complete: Boolean!) {
+    updateTodo(id: $id, complete: $complete)
+  }
+`;
+
 class App extends Component {
+  updateTodo = async todo => {
+    // update todo
+    await this.props.updateTodo({
+      variables: {
+        id: todo.id,
+        complete: !todo.complete
+      },
+      update: store => {
+        // Read the data from the cache for this query.
+        const data = store.readQuery({ query: TodosQuery });
+        // Update the data.
+        data.todos = data.todos.map(x =>
+          x.id === todo.id
+          ? {
+              ...todo,
+              complete: !todo.complete
+            }
+          : x
+        );
+        // Write data back to cache.
+        store.writeQuery({ query: TodosQuery, data });
+      }
+    })
+  };
+
+  removeTodo = async todo => {
+    // remove todo
+  };
+
   render() {
-    const { data: {loading, todos }} = this.props;
+    const {
+      data: {loading, todos }
+    } = this.props;
     if (loading) return null;
+
     return (
-      <div>
-        {todos.map(todo => <div key={`${todo.id}-todo-item`}>{todo.text}</div>)}
+      <div style={{ display: "flex"}}>
+        <div style={{ margin: "auto", width: 400 }}>
+          <Paper elevation={1}>
+            <List>
+              {todos.map(todo => (
+                <ListItem
+                  key={todo.id}
+                  role={undefined}
+                  dense
+                  button
+                  onClick={() => this.updateTodo(todo)}
+                >
+                  <Checkbox
+                    checked={todo.complete}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                  <ListItemText primary={todo.text} />
+                  <ListItemSecondaryAction>
+                    <IconButton onClick={() => this.removeTodo(todo)}>
+                      <CloseIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        </div>
       </div>
     );
   }
 }
 
-export default graphql(TodosQuery)(App);
+export default compose(
+  graphql(UpdateMutation, { name: "updateTodo"}),
+  graphql(TodosQuery)
+)(App);
